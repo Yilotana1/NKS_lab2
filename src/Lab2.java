ea1478abb1fda20af3ab1b1f2ec24ba18714b497
Yilotana1 <tolik.zhilko@gmail.com>
2022-04-13 22:40:14 +0300

    tested example data

--- src/Lab2.java
+++ src/Lab2.java
@@ -4,14 +4,16 @@ public class Lab2 {
 
 
     private static final Map<Integer, Collection<Integer>> systemGraph = new HashMap<>();
-    private static final double[] probabilities = new double[]{0.88, 0.14, 0.93, 0.04, 0.98, 0.56, 0.59, 0.64};
+    private static final double[] probabilities = new double[]{0.5, 0.6, 0.7, 0.8, 0.85, 0.9, 0.92, 0.94};
 
+
+    //Method to define system graph
     private static void dataInput() {
-        systemGraph.put(1, Arrays.asList(3, 4));
-        systemGraph.put(2, Arrays.asList(3, 5));
-        systemGraph.put(3, Arrays.asList(4, 5));
-        systemGraph.put(4, Arrays.asList(6, 7));
-        systemGraph.put(5, Arrays.asList(6, 8));
+        systemGraph.put(1, Arrays.asList(2, 3));
+        systemGraph.put(2, Arrays.asList(5, 4));
+        systemGraph.put(3, Arrays.asList(4, 6, 8));
+        systemGraph.put(4, Arrays.asList(5, 6, 8));
+        systemGraph.put(5, Arrays.asList(6, 7));
         systemGraph.put(6, Arrays.asList(7, 8));
         systemGraph.put(7, Arrays.asList());
         systemGraph.put(8, Arrays.asList());
